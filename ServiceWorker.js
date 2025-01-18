0dc2408617c9b6a579b729b11f8f9f15b4d75253
kalielson <kalielson@hotmail.com>
2025-01-18 18:02:40 -0300

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "Kalielso-Catalogo3Dmak-0.1";
-const contentToCache = [
-    "Build/Catalogo 3D MAK.loader.js",
-    "Build/Catalogo 3D MAK.framework.js.unityweb",
-    "Build/Catalogo 3D MAK.data.unityweb",
-    "Build/Catalogo 3D MAK.wasm.unityweb",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
